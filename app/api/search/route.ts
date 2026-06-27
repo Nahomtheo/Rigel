5cb8dbd3be37baf53d26f7e2745f2db4645d4e1e
Nahomtheo <nahomtheodros321@gmail.com>
2026-06-27 18:56:31 +0300

    relode2

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { connectDB } from '@/lib/db';
 import Listing from '@/models/Listing';
+import User from "@/models/User"; // add this when you want to populate the owner field with user details
 
 export async function GET(request: NextRequest) {
   try {
