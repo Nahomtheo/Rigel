13e7f0f787e542e8cab7b0c6cbf48254843978e4
Nahomtheo <nahomtheodros321@gmail.com>
2026-09-08 20:34:08 +0300

    net error

--- android/app/src/main/java/com/rigelcars/app/MainActivity.java
+++ android/app/src/main/java/com/rigelcars/app/MainActivity.java
@@ -1,5 +1,33 @@
 package com.rigelcars.app;
 
+import android.os.Bundle;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = getBridge().getWebView();
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onReceivedError(
+                    WebView view,
+                    WebResourceRequest request,
+                    WebResourceError error
+            ) {
+                if (request.isForMainFrame()) {
+                    view.loadUrl("file:///android_asset/offline.html");
+                }
+            }
+        });
+    }
+}
